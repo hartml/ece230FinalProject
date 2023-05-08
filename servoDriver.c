00a0cb9af2886265c473d573b329d97d37c5ba89
rhit-teaneyje <95186502+rhit-teaneyje@users.noreply.github.com>
2023-05-08 08:34:13 -0400

    Update servoDriver.c

--- servoDriver.c
+++ servoDriver.c
@@ -6,7 +6,8 @@
  *              Assumes SMCLK configured with 48MHz HFXT as source.
  *              Uses Timer_A2 and P5.6 (TA2.1)
  *
- *      Author: Matthew Hart
+ *  Created on:
+ *      Author:
  */
 
 #include "servoDriver.h"
@@ -16,14 +17,14 @@
 uint16_t pulseWidthTicks = SERVO_MIN_ANGLE;
 
 
-
 void initServoMotor(void) {
-    // configure servo pin (P5.6) for primary module function (TA2.1),
+    // DONE configure servo pin (P5.6) for primary module function (TA2.1),
     //  output, initially LOW
-    P5->SEL0 |= 0b01000000;
-    P5->SEL1 &= 0b10111111;
-    P5->DIR |=  0b01000000;
-    P5->OUT &=  0b10111111;
+    P5->SEL0 |= BIT6;
+    P5->SEL1 &= ~BIT6;
+    P5->OUT &= ~BIT6;
+    P5->DIR |= BIT6;
+
 
     /* Configure Timer_A2 and CCR1 */
     // Set period of Timer_A2 in CCR0 register for Up Mode
@@ -31,15 +32,26 @@ void initServoMotor(void) {
     // Set initial positive pulse-width of PWM in CCR1 register
     TIMER_A2->CCR[1] = SERVO_MIN_ANGLE;
 
-    // configure TA2CCR1 for Compare mode, Reset/Set output mode, with interrupt disabled
-    TIMER_A2->CCTL[1] = 0b0000000011100000;
-
-    // Configure Timer_A2 in Up Mode, with source SMCLK, prescale 64:1, and
-    //  interrupt disabled  -  tick rate will be 375kHz (for SMCLK = 48MHz)
-    // configure Timer_A2 (requires setting control AND expansion register)
-    TIMER_A2->CTL = 0b0000001011010000; // 1:8 prescale
-    TIMER_A2->EX0 = 0b0000000000000111; // 1:8 prescale expansion
+    // DONE configure TA2CCR1 for Compare mode, Reset/Set output mode, with interrupt disabled
+    TIMER_A2->CCTL[1] = 0x08E0; //0b 00xx 1xx0 1110 x000 = 0x08E0
+    // Configure Timer_A2 in Up Mode, with source SMCLK, prescale 8:1, and
+    //  interrupt disabled  -  tick rate will be 1MHz (for SMCLK = 48MHz)
+    // DONE configure Timer_A2 (requires setting control AND expansion register)
+    TIMER_A2->CTL = 0x0294; // 0b xxxx xx10 1001 x100 = 0x0294
+    TIMER_A2->EX0 |= 0x0004; //0b0100 = 0x0004 //So a total of 1:8:5 (1:40) pre-scale now
 }
 
+void incrementTenDegree(void) {
+    // update pulse-width for <current angle> + <10 degrees>
+    pulseWidthTicks += TEN_DEGREE_TICKS;
+    if (pulseWidthTicks > SERVO_MAX_ANGLE) {
+        pulseWidthTicks = SERVO_MIN_ANGLE;
+    }
+    // DONE update CCR1 register to set new positive pulse-width
+    TIMER_A2->CCR[1] = pulseWidthTicks;
+}
 
-
+void setServoAngle(uint8_t angle) {
+    // NOT NEEDED FOR EXERCISE - but would be useful function for driver
+    TIMER_A2->CCR[1] = SERVO_MIN_ANGLE + (angle * ONE_DEGREE_TICKS);
+}
