25c91b235bb6e6879ed989eaf64ae9adc99cdcc5
hartml <85324686+hartml@users.noreply.github.com>
2023-05-08 09:01:18 -0400

    basic changes

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include "msp.h"
+#include "servoDriver.h"
+#include "stepperMotor.h"
+#include "sysTickDelays.h"
+#include "csHFXT.h"
 #include <stdint.h>
 #include <stdio.h>
 
 
 void main(void) {
+    initServoMotor();      // initialize servo
+    initStepperMotor();    // initialize stepper
+    enableStepperMotor();  // enable stepper
+}
     while (1) {
 
-    } //end main loop
-} //end main
+   } //end main
